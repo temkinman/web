80ba29aeadc3ad1b22555f1f5cacbaeee979c4f1
Chegodar Artem <temkinman@mail.ru>
2020-05-04 18:57:25 +0300

    update CSS page

--- js/myscript.js
+++ js/myscript.js
@@ -69,6 +69,11 @@ function show(id){
   var alldiv = document.getElementsByClassName('d');
   var numLinkId = id.replace(/\D+/g,"");
   var arrLinks = document.getElementsByClassName('a');
+  var columns = 0;
+
+  if(document.getElementById(id).text == 'Двухколонный') {
+    columns = 1;
+  }
   
   for (let i=0; i<arrLinks.length;i++){
     arrLinks[i].style.fontWeight = 'normal';
@@ -78,13 +83,72 @@ function show(id){
   for(let i=0;i < alldiv.length;i++){
     var numDivId = alldiv[i].id.replace(/\D+/g,"");
     var mydiv = document.getElementById('div_' + numDivId);
+
     if (numDivId === numLinkId){
       mydiv.style.display = "block";
     }
     else {
       mydiv.style.display = "none";
+      // columns = 0;
+    }
+    if (columns == 1 && numDivId === numLinkId){
+      mydiv.style.display = "flex";
+      columns = 0;
     }
   }
 }
 
 /*for formatting_page end*/
+
+/*for CSS_page start*/
+
+  function select(id) {
+    switch (id) {
+      case '1': var x = document.getElementById('parentLi');
+      // debugger
+              // document.getElementsByClassName('psevdo').ul>li:first-child.style.background-color = '#3dab00';
+              break;
+      case '2': alert('2222');
+              break;
+              
+    }
+  }
+
+  function setStyle(){
+    let elements = document.getElementsByClassName('changeLi')[0].children;
+    let number = document.getElementById('userNumber').value;
+
+    elements[number-1].style.backgroundColor = 'white';
+    elements[number-1].style.color = 'black';
+    elements[number-1].style.border = '2px solid black';
+  }
+
+  function resetAll(){
+    let arr_checks = document.getElementsByClassName('checks');
+    let div = document.getElementsByClassName('number')[0];
+    let elementsli = document.getElementsByClassName('changeLi')[0].children;
+    
+    div.style.display = 'none';
+
+    for(let i=0; i<arr_checks.length; i++) {
+      arr_checks[i].checked = false;
+    }
+    arr_checks[7].checked = true;
+
+    for(let i=0; i<elementsli.length; i++) {
+      elementsli[i].style.backgroundColor = '';
+      elementsli[i].style.color = '';
+      elementsli[i].style.border = '';
+    }
+  }
+
+  function numberShow(){
+    let div = document.getElementsByClassName('number')[0];
+    div.style.display = 'flex';
+  }
+/*for CSS_page end*/
+
+/*for liveReload*/
+  //document.write('<script src="http://' + (location.host || 'localhost').split(':')[0] + ':35729/livereload.js?snipver=1"></' + 'script>');
+  document.write('<script src="http://' + (location.host || 'localhost').split(':')[0] + ':35729/livereload.js?snipver=1"></' + 'script>');
+/*end*/
