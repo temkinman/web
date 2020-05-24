dd86390e5d2758f9d3675bf87ba5e93710c7dec7
Chegodar Artem <temkinman@mail.ru>
2020-05-24 20:53:11 +0300

    krossword add main_table, left_table

--- js/myscript.js
+++ js/myscript.js
@@ -193,66 +193,146 @@ function show(id){
 /*for CSS_page end*/
 
 /*date_page start*/
-function tableCreate() {
+function create_table() {
   let main = document.getElementsByClassName('main-content')[0];
   let tbl = document.createElement('table');
   tbl.classList.add("nonogram_table");
   let tbdy = document.createElement('tbody');
-  let tr = document.createElement('tr');
-
+  
   for(var i=0; i<2; i++) {
-    let td = document.createElement('td');
-    if (i == 0) {
-      td.innerText = '\u00A0';
-      td.id = 'nmti';
-    }
-    else {
-      createtableAdd(td);
-    }
-    td.appendChild(document.createTextNode('\u0020'));
-    tr.appendChild(td);
+    let tr = document.createElement('tr');
+    createTr(tr, i);
+    
     tbdy.appendChild(tr);
   }
-
   tbl.appendChild(tbdy);
   main.appendChild(tbl);
 }
 
-function createtableAdd(td) {
-  let tbdy = document.createElement('tbody');
-  let tbl = document.createElement('table');
-  
-  let numbersY = [[3, 2, 6], [3, 3, 5, 4, 4, 5, 2], [6, 1, 4, 2, 3, 3, 2, 7, 5, 1, 2, 7, 1, 2, 2]];
-  
-  for(var i=0; i<6; i++){
+function createTr(tr, index) {
+  if (index == 0) {
+    for (var i=0; i<2; i++) {
+      let td = document.createElement('td');
+
+      if (i == 0) {
+        td.innerText = '\u00A0';
+        td.id = 'nmti';
+      }
+      else {
+        createtableTop(td);
+      }
+      // td.appendChild(document.createTextNode('\u0020'));
+      tr.appendChild(td);
+    }
+  }
+  if (index == 1) {
+    for (var i=0; i<2; i++) {
+        let td = document.createElement('td');
+        let tbl = document.createElement('table');
+        let tbdy = document.createElement('tbody');
+
+        if(i == 0) {
+          createtableLeft(tbdy);
+        }
+        else {
+          createtableMain(tbdy);
+        }
+        tbl.appendChild(tbdy);
+        td.appendChild(tbl);
+        tr.appendChild(td);
+    }
+  }
+}
+
+function createtableMain(tbdy) {
+  tbdy.classList.add("game_pic");
+  for(var i=0; i<30; i++){
     let tr = document.createElement('tr');
-    let a=0;
-    let b=0;
     for(var j=0;j<35;j++){
       let div = document.createElement('div');
       let td = document.createElement('td');
       
-      if( i == 0 && (j == 7 || j == 8 || j == 10)) {
-        div.innerText = numbersY[a][b];
-        b++;
-        div.id='nmv_' + i + '_' + j;
-        div.classList.add('num');  
-      }
-      else if ( i == 1 && (j == 3 || j == 7 || j == 8 || j == 10 || j == 13 || j == 14 || j == 28) ) {
-        a=1;
-        div.innerText = numbersY[a][b];
-        div.id='nmv_' + i + '_' + j;
-        div.classList.add('num'); 
-        b++;
-      }
-      else if ( i == 2 && (j == 1 || j == 3 || j == 4 || j == 7 || j == 8 || j == 9 || j == 10 || j == 11 || j == 12 || j == 13 || j == 14 || j == 15 || j == 17 || j == 26 || j == 28) ) {
-        a=2;
-        div.innerText = numbersY[a][b];
-        div.id='nmv_' + i + '_' + j;
-        div.classList.add('num'); 
-        b++;
-      }
+      div.innerText = '\u00A0'; 
+      div.id = 'nmf_' + i + '_' + j;
+      div.classList.add('pic');
+      div.setAttribute("oncontextmenu", "oncell_rightClick(event)");
+      div.setAttribute("onmousedown", "oncell_leftClick(event)");
+      
+      td.appendChild(div);
+      tr.appendChild(td);
+    }
+    tbdy.appendChild(tr);
+  }
+}
 
+function createtableTop(td) {
+  let tbdy = document.createElement('tbody');
+  let tbl = document.createElement('table');
+  tbl.id = 'topTable';
+  
+  let numbersY = [
+                  [7], [6, 1, 1, 5], [3, 2, 1], [3, 1, 5, 2, 2], [4, 1, 1, 4], [5, 3, 2],
+                  [7, 2, 14], [3, 3, 2, 3, 1, 5], [2, 5, 3, 2, 1, 2], [3, 6, 7, 4],
+                  [6, 4, 2, 4, 2, 2], [7, 1, 6, 3], [5, 1, 2, 8], [4, 1, 2, 2, 2], [5, 2, 2, 2, 2],
+                  [7, 2, 1, 4], [4, 2, 4], [1, 2, 2, 3], [1, 2, 3], [1, 3, 3], [1, 2], [1, 2], [1, 4], [1, 2],
+                  [2, 4, 2], [2, 5], [2, 1, 4, 2], [2, 3, 6], [2, 2, 3, 2, 2], [3, 3, 6], [3, 3, 2],
+                  [5, 2, 3], [3, 5, 7], [4, 9], [11]
+                ];
+
+  for (var i=0; i<6;i++){
+    let tr = document.createElement('tr');
+    for(var j=0;j<35;j++){
+      let div = document.createElement('div');
+      let td = document.createElement('td');
+      div.innerText = '\u00A0'; 
+      div.classList.add('num_empty');
+      td.appendChild(div);
+      tr.appendChild(td);
+    }
+    tbdy.appendChild(tr);
+  }
+  tbl.appendChild(tbdy);
+  td.appendChild(tbl);
+
+  
+  list= tbl.rows;
+  for (var j = 0; j < 35; j++) {
+    let b = 0;
+    for (var i = 0; i < 6; i++) {
+    if ( i >= 6 - numbersY[j].length ) {
+        let div = list[i].getElementsByTagName("td")[j].firstChild;
+        div.innerText = numbersY[j][b];
+        div.id='nmv_' + i + '_' + j;
+        div.classList.add('num');
+        b++;
+      }
+    }
+  }
+}
+
+function createtableLeft(tbdy) {
+  let numbersX = [
+                  [6], [9], [5, 4], [5, 5], [15], [2, 11], [1, 4, 1, 4], [2, 2, 3, 3], [2, 5,2],
+                  [2, 2, 2, 18], [1, 1, 1, 2, 9], [2, 2, 5], [1, 2, 1, 5], [4, 1, 2, 1, 1, 2],
+                  [2, 4, 1, 2, 1, 2], [2, 4, 1, 1, 1, 1], [3, 2, 1, 1, 1, 1, 1], [3, 2, 1, 1, 2, 1, 1],
+                  [2, 2, 1, 2, 2, 1, 1], [1, 2, 2, 2, 1, 1, 3], [1, 1, 2, 2, 3, 1, 1, 1, 1, 2],
+                  [1, 1, 1, 1, 1, 1, 1, 1, 3, 2], [2, 1, 1, 2, 1, 2, 2, 1, 2, 3], [1, 1, 2, 1, 2, 1, 13, 1, 2],
+                  [1, 1, 15, 4, 7], [2, 5, 3, 1, 1, 5], [5, 1, 1, 4, 1, 2], [4, 5, 3, 1, 3], [5, 5], [3, 4]
+                  ];
+
+  for(var i=0; i<30; i++){
+    let tr = document.createElement('tr');
+    let b=0;
+    for(var j=0;j<10;j++){
+      let div = document.createElement('div');
+      let td = document.createElement('td');
+      
+      if ( j >= 10 - numbersX[i].length ) {
+        div.innerText = numbersX[i][b];
+        div.id='nmh_' + i + '_' + j;
+        div.classList.add('num');
+        b++;
+      }
       else {
         div.innerText = '\u00A0'; 
         div.classList.add('num_empty'); 
@@ -263,8 +343,20 @@ function createtableAdd(td) {
     }
     tbdy.appendChild(tr);
   }
-  tbl.appendChild(tbdy);
-  td.appendChild(tbl);
+}
+
+function oncell_rightClick(e){
+  e.preventDefault();
+  e.target.style.fontSize='14px';
+  e.target.innerText = 'X'
+  console.log('Right click');
+}
+
+function oncell_leftClick(e){
+  e.preventDefault();
+  if (e.button == 0) {
+    e.target.style.backgroundColor = 'black';
+  }
 }
 /*date_page end*/
 
